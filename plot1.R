22b170aecd156016da0abc652427cdfd472f5444
Anat Demb <anatdemb@Anats-MacBook-Air.local>
2021-06-02 00:34:19 -0400

    Files for submission

---
+++ plot1.R
@@ -0,0 +1,16 @@
+### loading data 
+data <- read.table("household_power_consumption.txt", header= TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+summary(data)
+
+### subsetting the data 
+subsetdata <- data[data$Date %in% c("1/2/2007","2/2/2007"),]
+
+globalActivePower <- as.numeric(subsetdata$Global_active_power)
+globalReactivePower <- as.numeric(subsetdata$Global_reactive_power)
+voltage <- as.numeric(subsetdata$Voltage)
+subMetering1 <- as.numeric(subsetdata$Sub_metering_1)
+subMetering2 <- as.numeric(subsetdata$Sub_metering_2)
+subMetering3 <- as.numeric(subsetdata$Sub_metering_3)
+
+###plot 1- histogram
+hist(globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
